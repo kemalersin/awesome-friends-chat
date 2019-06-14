976d9e5f28bfb7ec54fee9afa2b6a9334b15085a
kemalersin <mail@kemalersin.com>
2019-06-14 16:31:50 +0300

    Slider added.

--- src/app/home/settings/settings.module.ts
+++ src/app/home/settings/settings.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { SettingsComponent } from './settings.component';
 
 @NgModule({
   declarations: [SettingsComponent],
-  imports: [BrowserModule, FormsModule, NgbModule],
+  imports: [BrowserModule, FormsModule, TranslateModule, NgbModule],
   exports: [SettingsComponent],
   providers: []
 })
